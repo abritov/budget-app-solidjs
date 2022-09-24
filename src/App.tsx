86f99b9da5484861108433337c7434ac21873bd9
abritov <pkgsail.dev@gmail.com>
2022-09-24 12:32:41 +0000

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,22 @@
 import { Component, createMemo } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
-import Paper from '@suid/material/Paper';
-import Card from '@suid/material/Card';
-import Typography from '@suid/material/Typography';
 import { createTheme, ThemeProvider } from '@suid/material';
+import { Link, Route, Routes } from '@solidjs/router';
+
+import Profile from './Profile';
+import Signin from './Signin';
 
 const App: Component = () => {
   const theme = createMemo(() => createTheme());
+  const loggedIn = false;
 
   return (
     <ThemeProvider theme={theme()}>
-      <div class={styles.App}>
-        <header class={styles.header}>
-          <img src={logo} class={styles.logo} alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-            <Paper>
-              <Card>
-                <Typography>Test</Typography>
-              </Card>
-            </Paper>
-          </p>
-          <a
-            class={styles.link}
-            href="https://github.com/solidjs/solid"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Solid
-          </a>
-        </header>
-      </div>
+      <Link href='/profile'>Profile page</Link>
+      <Link href='/signin'>Signin page</Link>
+      <Routes>
+        <Route path="/profile" component={Profile} />
+        <Route path="/signin" component={Signin} />
+      </Routes>
     </ThemeProvider>
   );
 };
---
+++ src/Profile.tsx
@@ -0,0 +1,13 @@
+import Paper from "@suid/material/Paper";
+import Typography from "@suid/material/Typography";
+import { Component } from "solid-js";
+
+const Profile: Component = () => {
+  return (
+    <Paper>
+      <Typography variant="body1">Basic profile page</Typography>
+    </Paper>
+  );
+};
+
+export default Profile;
